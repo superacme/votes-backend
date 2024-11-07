1fc2cc370d919bec37edd8aea755fb8ce6743e1e
Ainvoner <shaia@monada.co>
2024-11-07 15:10:17 +0200

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -3,6 +3,12 @@ import { config } from 'dotenv';
 
 config();
 
+console.log("host", process.env.POSTGRES_HOST)
+console.log("port", process.env.POSTGRES_PORT)
+console.log("user", process.env.POSTGRES_USER)
+console.log("pass",process.env.POSTGRES_PASSWORD)
+console.log("db", process.env.POSTGRES_DB)
+
 export const pool = new Pool({
     host: process.env.POSTGRES_HOST,
     port: Number(process.env.POSTGRES_PORT),
